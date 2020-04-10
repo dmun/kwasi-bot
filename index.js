79afd91d34e15043d4b9565f6e4572f29d8d32e1
kwertiee <davidru01@live.nl>
2020-04-10 09:15:00 +0200

    discord

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var PropertiesReader = require('properties-reader');
-var properties = PropertiesReader('properties.ini');
-var token = properties;
+//http://discordapp.com/oauth2/authorize?&client_id=697901929518334053&scope=bot&permissions=8
 
-var main = function() {
-    console.log(token);
-}
+const Discord = require('discord.js');
+const PropertiesReader = require('properties-reader');
 
-if (require.main === module) {
-    main();
-}
+const token = PropertiesReader('properties.ini').path().config.token;
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log("Connected");
+    bot.user.setPresence({
+        activity: {
+            name: 'IntelliJ IDEA'
+        },
+        status: 'available'
+    });
+});
+
+bot.login(token);
+
