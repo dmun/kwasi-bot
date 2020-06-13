af86a3554a573d3ca75c2f0856dea2c3af474830
kwertiee <davidru01@live.nl>
2020-06-13 22:11:30 +0200

    initial commit

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.content === prefix + 'kwasi') {
+    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === 'kwasi') {
         msg.reply('Hallo ik ben Kwasi!')
     }
 
-    if (msg.content === prefix + 'bke') {
+    if (command === 'bke') {
         var gameEmbed = new Discord.MessageEmbed()
             .setDescription('\:white_large_square: \:white_large_square: \:white_large_square:\n' +
                 '\:white_large_square: \:white_large_square: \:white_large_square:\n' +
@@ -32,8 +37,28 @@ client.on('message', msg => {
         msg.channel.send(gameEmbed);
     }
 
-    if (msg.content === prefix + 'take') {
-        
+    if (command === 'take') {
+        if (!args.length) {
+            msg.channel.send('No arguments provided.');
+        } else {
+            const mentioned = msg.mentions.users.first();
+            client.user.setAvatar(mentioned.avatarURL());
+            client.user.setUsername(mentioned.username());
+        }
+    }
+
+    if (command === 'clear') {
+        if (!args.length) {
+            msg.channel.send('No arguments provided.');
+        } else {
+            msg.channel.messages.fetch({ limit: parseInt(args[0]) + 1 }).then(messages => {
+                msg.channel.bulkDelete(messages);
+            });
+
+            msg.channel.send(args[0] + ' messages have been removed!').then(botmsg => {
+                botmsg.delete(3000);
+            });
+        }
     }
 });
 
